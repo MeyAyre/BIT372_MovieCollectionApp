e30804004e00dd3d1dc529d0debca5961606d530
Mei'lani <meyrehay@gmail.com>
2019-02-24 16:08:35 -0800

    Base has been built

---
+++ app/src/main/java/com/example/meiisreallycool/moviecollectionapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.meiisreallycool.moviecollectionapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
